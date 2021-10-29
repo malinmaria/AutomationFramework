da033b1c34c73b7a46bd27ed600523e35ee98a3c
malinmaria <malin.nemergut@gmail.com>
2021-10-29 17:01:35 +0100

    Creating locators and methods

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -8,6 +8,37 @@ class LoginPage():
     def __init__(self, driver):
         self.driver = driver
 
+    #Locators
+    _login_link = "Login"
+    _email_field = "user_email"
+    _password_field = "user_password"
+    _login_button = "commit"
+
+    def getLoginLink(self):
+        return self.driver.find_element(By.LINK_TEXT, self._login_link)
+
+    def getEmailField(self):
+        return self.driver.find_element(By.ID, self._email_field)
+
+    def getPasswordField(self):
+        return self.driver.find_element(By.ID, self._password_field)
+
+    def getLoginButton(self):
+        return self.driver.find_element(By.NAME, self._login_button)
+
+    #Actions
+    def clickLoginLink(self):
+        self.getLoginLink().click()
+
+    def enterEmail(self, email):
+        self.getEmailField().send_keys(email)
+
+    def enterPassword(self, password):
+        self.getPasswordField().send_keys(password)
+
+    def clickLoginButton(self):
+        self.getLoginButton().click()
+
     def login(self, username, password):
         loginLink = self.driver.find_element(By.LINK_TEXT, "login")
         loginLink.click()
